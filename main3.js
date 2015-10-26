6660316ff8c8f27a14bb67ae0855d5570821e658
Yuki Ban <yuki@xcoo.jp>
2015-10-26 19:50:45 +0900

    first commit

---
+++ main3.js
@@ -0,0 +1,28 @@
+var splainfo = require('./splainfo')
+
+var si = splainfo();
+
+si.on('done', function(){
+    res = "ハイカラニュースの時間だよ!\n\n";
+    if(this.fes_state > -1) {
+        state = ["告知", "開催"][this.fes_state];
+        res += "フェスが" + state + "されているよ\n";
+        res += "期間は" + this.fes.begin + " 〜 " + this.fes.end + "だよ\n";
+        res += "お題は " + this.fes.alpha + " 対 " + this.fes.bravo + " だよ\n";
+    }
+
+    if(this.fes_state == 1) {
+        res += "レギュラーは " + this.regular[0] + " と " + this.regular[1] + " と " + this.regular[2] + " だよ\n\n"
+    } else {
+        res += "\n";
+        objDate = new Date(this.starttime);
+        res += objDate.getFullYear() + "/" + (objDate.getMonth()+1) + "/" + ("0" + objDate.getDate()).slice(-2) + " ";
+        res += ("0" + objDate.getHours()).slice(-2) +":" + ("0" + objDate.getMinutes()).slice(-2)  + "からは\n";
+        res +=  "レギュラーは " + this.regular[0] + " と " + this.regular[1] + "，\n";
+        res +=  "ガチマッチは " + this.ranked[0] + " と " + this.ranked[1] + " の " + this.ranked_rule + " だよ\n\n";
+    }
+    res += "イカよろしくー\n";
+    console.log(res);
+});
+
+
---
+++ splainfo/index.js
@@ -0,0 +1,85 @@
+var https = require('https');
+var http = require('http');
+var events = require('events')
+
+module.exports = function () {
+
+	var splainfo = new events.EventEmitter();
+	splainfo.fes_url = 'http://s3-ap-northeast-1.amazonaws.com/splatoon-data.nintendo.net/fes_info.json';
+	splainfo.ink_url = 'https://splatoon.ink/schedule.json';
+	splainfo.fes_state = -1;
+	splainfo.fes = {};
+	splainfo.regular = new Array();
+	splainfo.ranked = new Array();
+	splainfo.ranked_rule = "";
+
+	http.get(splainfo.fes_url, function(res){
+		var body = '';
+		res.setEncoding('utf8');
+
+		res.on('data', function(chunk){
+			body += chunk;
+		});
+
+		res.on('end', function(res){
+			ret = JSON.parse(body);
+			splainfo.fes_state = ret.fes_state;
+			splainfo.fes["begin"] = ret.datetime_fes_begin;
+			splainfo.fes["end"] = ret.datetime_fes_end;
+			splainfo.fes["alpha"] = ret.team_alpha_name;
+			splainfo.fes["bravo"] = ret.team_bravo_name;
+			if (splainfo.fes_state > 0) {
+				splainfo.regular.push(ret["fes_stage"][0]["name"]);
+				splainfo.regular.push(ret["fes_stage"][1]["name"]);
+				splainfo.regular.push(ret["fes_stage"][2]["name"]);
+			}
+
+			if(splainfo.fes_state != 1) {
+
+				https.get(splainfo.ink_url, function(res){
+					var body = '';
+					res.setEncoding('utf8');
+
+					res.on('data', function(chunk){
+						body += chunk;
+					});
+
+					res.on('end', function(res){
+						json_info = JSON.parse(body);
+						now_time = (new Date()).getTime();
+						var resnum = 0;
+						for(var i=0; i<json_info.schedule.length; i++) {
+							if (json_info.schedule[i].startTime <= now_time && json_info.schedule[i].endTime >= now_time)
+								resnum = i;
+						}
+
+						var result = json_info.schedule[resnum];
+						var regular = result.regular;
+						var ranked = result.ranked;
+
+						splainfo.starttime = result.startTime;
+						splainfo.endtime = result.endTime;
+						splainfo.regular.push(regular.maps[0]["nameJP"]);
+						splainfo.regular.push(regular.maps[1]["nameJP"]);
+						splainfo.ranked.push(ranked.maps[0]["nameJP"]);
+						splainfo.ranked.push(ranked.maps[1]["nameJP"]);
+						splainfo.ranked_rule = ranked.rulesJP;
+
+						splainfo.emit('done');
+
+					});
+				}).on('error', function(e){
+					console.log(e.message); 
+				});
+			}
+
+
+		});
+}).on('error', function(e){
+	console.log(e.message); 
+});
+
+return splainfo;
+
+}
+
